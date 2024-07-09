6c19a6d59d45b16f06be4198a5a60e88579d96ca
Anvill1 <rtav3d@gmail.com>
2024-07-09 12:15:24 +0300

    style: изменил endpoint c /deploys/create на /deployment/create

--- api/routes.go
+++ api/routes.go
@@ -8,7 +8,7 @@ import (
 
 func CreateRoutes(deployHandler *handlers.DeployHandler) *mux.Router {
 	r := mux.NewRouter()
-	r.HandleFunc("/api/deploys/create", deployHandler.Create).Methods("POST")
+	r.HandleFunc("/api/deployment/create", deployHandler.Create).Methods("POST")
 	r.NotFoundHandler = r.NewRoute().HandlerFunc(handlers.NotFound).GetHandler()
 	return r
 }
